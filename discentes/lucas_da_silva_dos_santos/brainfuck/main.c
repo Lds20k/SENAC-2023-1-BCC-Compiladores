0fbac050262edc60a8cfe7ea0505cf677d324ab0
Lucas da Silva dos Santos <lucassilva.lds@outlook.com>
2023-03-12 00:15:02 -0300

    feat: argumento de local de arquivo para o interpretador

--- discentes/lucas_da_silva_dos_santos/brainfuck/main.c
+++ discentes/lucas_da_silva_dos_santos/brainfuck/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define FILE_NOT_FOUND "file not found!"
+
 int main(int argc, char const *argv[])
 {
+    if(argc < 2){
+        fprintf(stderr, "error: %s\n", FILE_NOT_FOUND);
+        return -1;
+    }
+
+    printf("%s\n", argv[1]);
+
     return 0;
 }
