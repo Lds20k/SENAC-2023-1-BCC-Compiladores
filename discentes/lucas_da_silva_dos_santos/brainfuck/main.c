8e1ea2eac6584f1ee5cd4030b7a45aa054495f00
Lucas da Silva dos Santos <lucassilva.lds@outlook.com>
2023-03-12 00:28:55 -0300

    feat: abertura de arquivo

--- discentes/lucas_da_silva_dos_santos/brainfuck/main.c
+++ discentes/lucas_da_silva_dos_santos/brainfuck/main.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 #define FILE_NOT_FOUND "file not found!"
 
 int main(int argc, char const *argv[])
 {
     if(argc < 2){
-        fprintf(stderr, "error: %s\n", FILE_NOT_FOUND);
-        return -1;
+        fprintf(stderr, "[arguments error] %s\n", FILE_NOT_FOUND);
+        return 1;
     }
 
-    printf("%s\n", argv[1]);
+    FILE* file_pointer = fopen(argv[1], "r");
+    if (file_pointer == NULL)
+    {
+        int errnum = errno;
+        fprintf(stderr, "[error opening file] %s\n", strerror(errnum));
+        return 1;
+    }
+    
+    fclose(file_pointer);
 
     return 0;
 }
