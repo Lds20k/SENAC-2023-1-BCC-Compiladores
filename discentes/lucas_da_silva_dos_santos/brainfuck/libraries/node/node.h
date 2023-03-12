a2d9205cebb974664b12deea848097ec7426f969
Lucas da Silva dos Santos <lucassilva.lds@outlook.com>
2023-03-11 21:10:16 -0300

    feat: definição de node

---
+++ discentes/lucas_da_silva_dos_santos/brainfuck/libraries/node/node.h
@@ -0,0 +1,15 @@
+#ifndef NODE_H
+#define NODE_H
+#include <inttypes.h>
+
+typedef struct node_s
+{
+    char* data_type;
+    void* data;
+    uint32_t edges_size;
+    node_t* edges;
+} node_t;
+
+node_t* create_node();
+
+#endif
